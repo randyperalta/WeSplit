111c9eb492114c3a448d0c0638b15d09237cfb03
Randy Peralta <randyperalta@outlook.com>
2023-04-09 13:56:49 -0400

    Creating a form

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -9,8 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Form {
+            Section {
+                Text("Hello, world!")
+            }
+            Section {
+                Text("Hello, world!")
+                Text("Hello, world!")
+            }
+        }
+            
     }
 }
 
